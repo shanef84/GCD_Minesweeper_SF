a56bbf84c90bc3578a950e55fe0b034902f326c6
shanef84 <shane.forbes@student.gcd.ie>
2016-04-03 16:21:31 +0100

    02
    Force the custom view to restrict itself to a square size

--- app/src/main/java/com/example/forbess/gcd_minesweeper_sf/CustomView.java
+++ app/src/main/java/com/example/forbess/gcd_minesweeper_sf/CustomView.java
@@ -31,6 +31,26 @@ public class CustomView extends View{
         super(c, as, default_style);
         init();
     }
+
+    //override the onMeasure() method to make CustomView a square
+    @Override
+    protected void onMeasure(int widthMeasure, int heightMeasure) {
+        super.onMeasure(widthMeasure, heightMeasure);
+
+        int size =0;
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+        int widthPadding = width - getPaddingLeft() - getPaddingRight();
+        int heightPadding = height - getPaddingTop() - getPaddingBottom();
+        //set dimensions
+        if (widthPadding > heightPadding) {
+            size = heightPadding;
+        } else {
+            size = widthPadding;
+        }
+        setMeasuredDimension(size + getPaddingLeft() + getPaddingRight(), size + getPaddingTop() + getPaddingBottom());
+    }
+
     // refactored init method as most of this code is shared by all the constructors
     private void init() {
     }
--- app/src/main/java/com/example/forbess/gcd_minesweeper_sf/MainActivity.java
+++ app/src/main/java/com/example/forbess/gcd_minesweeper_sf/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -48,5 +49,52 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    // overridden destroy method that should be used to free up memory as this
+    // activity is being destroyed
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MainActivity", "onDestroy() called");
+    }
+
+    // overridden resume method that should be used to restart threads and processing
+    // when the activity becomes fullscreen and unobstructed. this method should be
+    // quick at processing because it has the potential to be called many times in a
+    // single activity
+    protected void onResume() {
+        super.onResume();
+        Log.i("MainActivity", "onResume() called");
+    }
+
+    // overridden pause method that should be used to temporarily stop threads and
+    // processing when the activity has become partially obstructed (e.g. a dialog
+    // potential to be called many times in a single activity
+    protected void onPause() {
+        super.onPause();
+        Log.i("MainActivity", "onPause() called");
+    }
+
+    // overridden stop method that is used to temporarily free up resources for other
+    // activities and applications. This is called whenever the activity becomes
+    // completely obscured or backgrounded.
+    protected void onStop() {
+        super.onStop();
+        Log.i("MainActivity", "onStop() called");
+    }
+
+    // overridden restart method that is used to restart an activity. most of the time
+    // you can ignore this method as the on start method will be called imediately
+    // after this method. It remains for compatibility purposes
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("MainActivity", "onRestart() called");
+    }
+
+    // overridden start method that is used to allocate resources to the activity both
+    // when it is created or when it is being restarted from a stopped state. Any
+    // memory freed from onStop should be reallocated here
+    protected void onStart() {
+        super.onStart();
+        Log.i("MainActivity", "onStart() called");
     }
 }
